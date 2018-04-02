d68e02c2f53e92f40f5cd1956d956bffb0c2f29c
stormning <stormning@163.com>
2018-04-02 23:34:48 +0800

    zzbid

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 export TESSDATA_PREFIX=/usr/local/share
 export GIT_SSL_NO_VERIFY=true
-GIT_URL=git@github.com:stormning/zzbid.git
+GIT_URL=https://github.com/stormning/zzbid.git
 ZZBID_SRC=/opt/zzbid/source
 if [[ -d $ZZBID_SRC ]];then
-    mkdir $ZZBID_SRC
     cd $ZZBID_SRC
     git clone $GIT_URL .
     git remote add origin $GIT_URL
