9a8fe829bb6216ba75d127e559599d33b74d6f62
stormning <stormning@163.com>
2018-04-03 00:26:25 +0800

    release

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,15 @@ export TESSDATA_PREFIX=/usr/local/share
 export GIT_SSL_NO_VERIFY=true
 GIT_URL=https://github.com/stormning/zzbid.git
 ZZBID_SRC=/opt/zzbid/source
-if [[ -d $ZZBID_SRC ]];then
+if [ -f $ZZBID_SRC/pom.xml ];then
+    echo "start resetting ..."
+    cd $ZZBID_SRC
+    git reset --hard origin/master
+    else
+    echo "start cloning ..."
     cd $ZZBID_SRC
     git clone $GIT_URL .
     git remote add origin $GIT_URL
-    else
-    cd $ZZBID_SRC
-    git reset --hard origin/master
 fi
 $ZZBID_SRC/mvnw clean install
 $ZZBID_SRC/mvnw spring-boot:run
